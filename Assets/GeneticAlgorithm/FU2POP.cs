0f23a9c6dc9ed98300800999b0de0f47a32d2b31
Charlie Ringer <cring002@gold.ac.uk>
2018-06-01 00:07:41 +0300

    Now tracks that are generated are always feasible

--- Assets/GeneticAlgorithm/FU2POP.cs
+++ Assets/GeneticAlgorithm/FU2POP.cs
@@ -2,73 +2,104 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FU2POP {
+public class FU2POP
+{
 
     public static int pop_size = 10;
     public List<TrackChromosome> pop;
 
-    public List<TrackChromosome> feasable;
-    public List<TrackChromosome> unfeasable;
+    public List<TrackChromosome> feasable = new List<TrackChromosome>();
+    public List<TrackChromosome> unfeasable = new List<TrackChromosome>();
 
-    public FU2POP(int track_length){
+    public FU2POP(int track_length)
+    {
         pop = new List<TrackChromosome>();
 
-        for (int i = 0; i < pop_size; i++){
+        for (int i = 0; i < pop_size; i++)
+        {
             pop.Add(new TrackChromosome(track_length));
         }
+        FeasibilityCheck();
     }
 
-    public void SetIsFeasable(bool isFeasable, int index){
+    public void SetIsFeasable(bool isFeasable, int index)
+    {
         pop[index].SetFeasable(isFeasable);
     }
 
-    public void SetFitness(int fitness, int index){
+    public void SetFitness(int fitness, int index)
+    {
         pop[index].SetFitness(fitness);
     }
 
-    public void Evolve(){
-        feasable.Clear();
-        unfeasable.Clear();
 
-        for (int i = 0; i < pop_size; i++){
-            if(pop[i].IsFeasable()){
+    public void FeasibilityCheck()
+    {
+
+        // check feasibility
+        foreach(TrackChromosome c in pop)
+        {
+            c.SetFeasable(TrackLoader.Check(c));
+        }
+
+        for (int i = 0; i < pop_size; i++)
+        {
+            if (pop[i].IsFeasable())
+            {
                 feasable.Add(pop[i]);
-            }else{
+            }
+            else
+            {
                 unfeasable.Add(pop[i]);
             }
         }
+    }
+
+    public void Evolve()
+    {
+
 
         int from_feasable = pop_size / 2;
-        int from_unfeasable = pop_size-from_feasable;
+        int from_unfeasable = pop_size - from_feasable;
 
         List<TrackChromosome> new_pop = new List<TrackChromosome>();
-        new_pop.AddRange(NewPopulation(feasable,from_feasable));
+        new_pop.AddRange(NewPopulation(feasable, from_feasable));
         new_pop.AddRange(NewPopulation(unfeasable, from_unfeasable));
 
         pop.Clear();
         pop.AddRange(new_pop);
 
+        feasable.Clear();
+        unfeasable.Clear();
+
+        FeasibilityCheck();
+
     }
 
-    private TrackChromosome Selection(List<TrackChromosome> chrs){
+    private TrackChromosome Selection(List<TrackChromosome> chrs)
+    {
         TrackChromosome s = chrs[Random.Range(0, chrs.Count)];
         int tournament_size = 4;
 
-        for (int i = 1; i < tournament_size; i++){
-            TrackChromosome new_s = chrs[Random.Range(0,chrs.Count)];
-            if(s.GetFitness() < new_s.GetFitness()){
+        for (int i = 1; i < tournament_size; i++)
+        {
+            TrackChromosome new_s = chrs[Random.Range(0, chrs.Count)];
+            if (s.GetFitness() < new_s.GetFitness())
+            {
                 s = new_s;
             }
         }
         return s;
     }
 
-    private List<TrackChromosome> NewPopulation(List<TrackChromosome> old_pop, int new_size){
+    private List<TrackChromosome> NewPopulation(List<TrackChromosome> old_pop, int new_size)
+    {
         List<TrackChromosome> new_pop = new List<TrackChromosome>();
 
 
 
-        while(new_pop.Count != new_size){
+        while (new_pop.Count != new_size)
+        {
             TrackChromosome c1 = Selection(old_pop);
             TrackChromosome c2 = Selection(old_pop);
 
@@ -76,13 +107,15 @@ public class FU2POP {
 
             new_pop.Add(crossed[0]);
 
-            if(new_pop.Count < new_size){
+            if (new_pop.Count < new_size)
+            {
                 new_pop.Add(crossed[1]);
             }
 
         }
 
-        for (int i = 0; i < new_size; i++){
+        for (int i = 0; i < new_size; i++)
+        {
             new_pop[i].Mutate();
         }
 
--- Assets/TrackLoader.cs
+++ Assets/TrackLoader.cs
@@ -19,7 +19,7 @@ public class TrackLoader : MonoBehaviour {
 	{
         trackPopulation = new FU2POP(15);
 
-        loadTrack(trackPopulation.pop[currentTrack].GetGenes());
+        loadTrack(trackPopulation.feasable[currentTrack].GetGenes());
 	}
 
 
@@ -35,11 +35,11 @@ public class TrackLoader : MonoBehaviour {
             updateTrackFitness();
 
             timeLeft = trackTime;
-            if(currentTrack > trackPopulation.pop.Count)
+            if(currentTrack > trackPopulation.feasable.Count)
             {
                 //if currentTrack > length of track pop then generate next pop 
             } else {
-                loadTrack(trackPopulation.pop[currentTrack].GetGenes());
+                loadTrack(trackPopulation.feasable[currentTrack].GetGenes());
             }
             trackManIterations++;
         }
@@ -180,5 +180,129 @@ public class TrackLoader : MonoBehaviour {
             coords.Add(nextCoords);
         }
     }
+
+    public static bool Check( TrackChromosome t)
+    {
+        int[] track = t.GetGenes();
+        //0 = hard right, 1 = hard left, 2 = soft right, 3 = soft left, 4 = straight, 5 = end
+        //remove old track
+
+        List<int[]> coords = new List<int[]>();
+
+        int entryDir = 0;
+
+        int[] startingCoords = { 0, 0 };
+        coords.Add(startingCoords);
+
+        foreach (int trackElemt in track)
+        {
+            int[] latestCoords = coords[coords.Count - 1];
+            Quaternion rotation = new Quaternion();
+            if (entryDir == 0) rotation = Quaternion.Euler(0f, 0f, 0f);
+            else if (entryDir == 1) rotation = Quaternion.Euler(0f, 90f, 0f);
+            else if (entryDir == 2) rotation = Quaternion.Euler(0f, 180f, 0f);
+            else if (entryDir == 3) rotation = Quaternion.Euler(0f, 270f, 0f);
+
+            int[] nextCoords = new int[2];
+
+            if (entryDir == 0)
+            {
+                if (trackElemt == 0 || trackElemt == 2)
+                {
+                    nextCoords[0] = latestCoords[0] + 1;
+                    nextCoords[1] = latestCoords[1];
+                    entryDir = 1;
+                }
+                else if (trackElemt == 1 || trackElemt == 3)
+                {
+                    nextCoords[0] = latestCoords[0] - 1;
+                    nextCoords[1] = latestCoords[1];
+                    entryDir = 3;
+
+                }
+                else if (trackElemt == 4 || trackElemt == 5)
+                {
+                    nextCoords[0] = latestCoords[0];
+                    nextCoords[1] = latestCoords[1] + 1;
+                    entryDir = 0;
+                }
+            }
+            else if (entryDir == 1)
+            {
+                if (trackElemt == 0 || trackElemt == 2)
+                {
+                    nextCoords[0] = latestCoords[0];
+                    nextCoords[1] = latestCoords[1] - 1;
+                    entryDir = 2;
+                }
+                else if (trackElemt == 1 || trackElemt == 3)
+                {
+                    nextCoords[0] = latestCoords[0];
+                    nextCoords[1] = latestCoords[1] + 1;
+                    entryDir = 0;
+
+                }
+                else if (trackElemt == 4 || trackElemt == 5)
+                {
+                    nextCoords[0] = latestCoords[0] + 1;
+                    nextCoords[1] = latestCoords[1];
+                    entryDir = 1;
+                }
+
+            }
+            else if (entryDir == 2)
+            {
+                if (trackElemt == 0 || trackElemt == 2)
+                {
+                    nextCoords[0] = latestCoords[0] - 1;
+                    nextCoords[1] = latestCoords[1];
+                    entryDir = 3;
+                }
+                else if (trackElemt == 1 || trackElemt == 3)
+                {
+                    nextCoords[0] = latestCoords[0] + 1;
+                    nextCoords[1] = latestCoords[1];
+                    entryDir = 1;
+
+                }
+                else if (trackElemt == 4 || trackElemt == 5)
+                {
+                    nextCoords[0] = latestCoords[0];
+                    nextCoords[1] = latestCoords[1] - 1;
+                    entryDir = 2;
+                }
+            }
+            else if (entryDir == 3)
+            {
+                if (trackElemt == 0 || trackElemt == 2)
+                {
+                    nextCoords[0] = latestCoords[0];
+                    nextCoords[1] = latestCoords[1] + 1;
+                    entryDir = 0;
+                }
+                else if (trackElemt == 1 || trackElemt == 3)
+                {
+                    nextCoords[0] = latestCoords[0];
+                    nextCoords[1] = latestCoords[1] - 1;
+                    entryDir = 2;
+
+                }
+                else if (trackElemt == 4 || trackElemt == 5)
+                {
+                    nextCoords[0] = latestCoords[0] - 1;
+                    nextCoords[1] = latestCoords[1];
+                    entryDir = 3;
+                }
+            }
+            foreach (int[] prevCoord in coords)
+            {
+                if (prevCoord[0] == nextCoords[0] && prevCoord[1] == nextCoords[1]) return false;
+
+            }
+            coords.Add(nextCoords);
+        }
+        return true;
+    }
+
 
 }
