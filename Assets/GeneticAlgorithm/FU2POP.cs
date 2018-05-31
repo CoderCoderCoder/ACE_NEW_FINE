72576ab17058ad4b4d5f440493d09f6378e66147
Ivan Bravi <ivan.bravi@gmail.com>
2018-05-31 19:19:03 +0300

    FI2POP implemented

---
+++ Assets/GeneticAlgorithm/FU2POP.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FU2POP {
+
+    public static int pop_size = 10;
+    public List<TrackChromosome> pop;
+
+    public List<TrackChromosome> feasable;
+    public List<TrackChromosome> unfeasable;
+
+    public FU2POP(int track_length){
+        pop = new List<TrackChromosome>();
+
+        for (int i = 0; i < pop_size; i++){
+            pop.Add(new TrackChromosome(track_length));
+        }
+    }
+
+    public void SetIsFeasable(bool isFeasable, int index){
+        pop[index].SetFeasable(isFeasable);
+    }
+
+    public void SetFitness(int fitness, int index){
+        pop[index].SetFitness(fitness);
+    }
+
+    public void Evolve(){
+        feasable.Clear();
+        unfeasable.Clear();
+
+        for (int i = 0; i < pop_size; i++){
+            if(pop[i].IsFeasable()){
+                feasable.Add(pop[i]);
+            }else{
+                unfeasable.Add(pop[i]);
+            }
+        }
+
+        int from_feasable = pop_size / 2;
+        int from_unfeasable = pop_size-from_feasable;
+
+        List<TrackChromosome> new_pop = new List<TrackChromosome>();
+        new_pop.AddRange(NewPopulation(feasable,from_feasable));
+        new_pop.AddRange(NewPopulation(unfeasable, from_unfeasable));
+
+        pop.Clear();
+        pop.AddRange(new_pop);
+
+    }
+
+    private TrackChromosome Selection(List<TrackChromosome> chrs){
+        TrackChromosome s = chrs[Random.Range(0, chrs.Count)];
+        int tournament_size = 4;
+
+        for (int i = 1; i < tournament_size; i++){
+            TrackChromosome new_s = chrs[Random.Range(0,chrs.Count)];
+            if(s.GetFitness() < new_s.GetFitness()){
+                s = new_s;
+            }
+        }
+        return s;
+    }
+
+    private List<TrackChromosome> NewPopulation(List<TrackChromosome> old_pop, int new_size){
+        List<TrackChromosome> new_pop = new List<TrackChromosome>();
+
+
+
+        while(new_pop.Count != new_size){
+            TrackChromosome c1 = Selection(old_pop);
+            TrackChromosome c2 = Selection(old_pop);
+
+            List<TrackChromosome> crossed = TrackChromosome.Crossover(c1, c2);
+
+            new_pop.Add(crossed[0]);
+
+            if(new_pop.Count < new_size){
+                new_pop.Add(crossed[1]);
+            }
+
+        }
+
+        for (int i = 0; i < new_size; i++){
+            new_pop[i].Mutate();
+        }
+
+        return new_pop;
+    }
+
+}
---
+++ Assets/GeneticAlgorithm/TrackChromosome.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackChromosome{
+
+    private static int max_gene;
+    private int[] genes;
+    private float fitness;
+    private bool is_feasable;
+
+    public TrackChromosome(int size){
+        genes = new int[size];
+        RandomInit();
+    }
+
+    private void RandomInit(){
+        for (int i = 0; i < genes.Length; i++){
+            genes[i] = RandomGene();
+        }
+    }
+
+    public void SetFeasable(bool is_feasable){
+        this.is_feasable = is_feasable;
+    }
+
+    public bool IsFeasable(){
+        return is_feasable;
+    }
+
+    private static int RandomGene(){
+        return Random.Range(0, max_gene + 1);
+    }
+
+    public void SetFitness(float fitness){
+        this.fitness = fitness;
+    }
+
+    public float GetFitness(){
+        return this.fitness;
+    }
+
+    public TrackChromosome(TrackChromosome c){
+        genes = new int[c.genes.Length];
+        for (int i = 0; i < c.genes.Length; i++)
+        {
+            genes[i] = c.genes[i];
+        }
+    }
+
+    public void Mutate(){
+        genes[Random.Range(0, genes.Length)] = RandomGene();         
+    }
+
+    public static List<TrackChromosome> Crossover(TrackChromosome c1, TrackChromosome c2){
+        List<TrackChromosome> new_cromosomes = new List<TrackChromosome>();
+
+        TrackChromosome new_c1 = new TrackChromosome(c1);
+        TrackChromosome new_c2 = new TrackChromosome(c2);
+
+        int x_point = Random.Range(0, c1.genes.Length);
+
+        for (int i = 0; i < c1.genes.Length; i++){
+            if(i<x_point){
+                new_c1.genes[i] = c2.genes[i];
+            }else{
+                new_c2.genes[i] = c1.genes[i];
+            }
+        }
+
+        new_cromosomes.Add(new_c1);
+        new_cromosomes.Add(new_c2);
+
+        return new_cromosomes;
+    }
+
+    public int GetGene(int index){
+        if(OutOfRange(index)){
+            return -1;    
+        }
+        return this.genes[index];
+    }
+
+    public bool OutOfRange(int index){
+        return index < 0 || index > genes.Length - 1;
+    }
+
+    public bool IsGeneOk(int gene_value){
+        return gene_value >= 0 && gene_value <= max_gene;
+    }
+
+}
